145b604028a0cabe0da21e8f20b29e5ed4880081
Vidal Carro <vidal.carro@gmail.com>
2018-06-23 13:44:14 +0200

    Let a user to choose with what hero goes to play

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -6,10 +6,16 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     Shell.info("Start by choosing your hero:")
 
     heroes = DungeonCrawl.Heroes.all()
+    find_hero_by_index = &Enum.at(heroes, &1)
 
     heroes
     |> Enum.map(&(&1.name))
     |> display_options
+    |> generate_question
+    |> Shell.prompt
+    |> parse_answer
+    |> find_hero_by_index.()
+    |> confirm_hero
   end
 
   def display_options(options) do
@@ -20,6 +26,22 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     end)
 
     options
+  end
+
+  defp generate_question(options) do
+    options = Enum.join(1..Enum.count(options),",")
+    "Which one? [#{options}]\n"
+  end
+
+  defp parse_answer(answer) do
+    {option, _} = Integer.parse(answer)
+    option - 1
+  end
+
+  defp confirm_hero(chosen_hero) do
+    Shell.cmd("clear")
+    Shell.info(chosen_hero.description)
+    if Shell.yes?("Confirm?"), do: chosen_hero, else: start()
   end
 
 end
